1e180dada65426e09d066c9fabc8c1fd27408055
Zsolt Fekete <zsoltfekete.zsf@gmail.com>
2017-05-08 17:50:26 +0200

    init

---
+++ vue-cli/src/components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <nav class="navbar navbar-default">
+    <div class="container-fluid">
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <ul class="nav navbar-nav">
+          <router-link to="/portfolio" activeClass="active" tag="li"><a>Portfolio</a></router-link>
+          <router-link to="/stocks" activeClass="active" tag="li"><a>Stocks</a></router-link>
+        </ul>
+        <ul class="nav navbar-nav navbar-right">
+          <li><a href="#">End day</a></li>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Save & Load <span class="caret"></span></a>
+            <ul class="dropdown-menu">
+              <li><a href="#">Save data</a></li>
+              <li><a href="#">Load data</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
---
+++ vue-cli/src/components/stocks/Stocks.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="col-sm-12 col-md-12">
+    <app-stock v-for="stock in stocks" :key="stock.id" :stock="stock"></app-stock>
+  </div>
+</template>
+
+<script>
+  import Stock from './Stock.vue'
+
+  export default {
+    data() {
+      return {
+        stocks:[
+          {id: 1, name: 'Google', price: 110},
+          {id: 2, name: 'BMW', price: 200},
+          {id: 3, name: 'Apple', price: 250},
+          {id: 4, name: 'Twitter', price: 80},
+        ]
+      }
+    },
+    components:{
+      appStock: Stock
+    }
+  }
+</script>
